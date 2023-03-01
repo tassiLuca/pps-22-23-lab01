ffe4ae00b735059b6bd2d5e6c9b974d0abc1b586
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-01 10:36:22 +0100

    feat: add CircularList interface step2

---
+++ pps-lab01-tdd/src/lab01/tdd/step2/CircularList.java
@@ -0,0 +1,44 @@
+package lab01.tdd.step2;
+
+import java.util.Iterator;
+
+/**
+ * Represents a list of integers, which provides {@link Iterator} in order to iterate elements
+ * forward and backward circularly.
+ */
+public interface CircularList {
+
+    /**
+     * Adds an element to the list, namely, after the last inserted one.
+     * @param element the element to be added to the list
+     */
+    void add(final int element);
+
+    /**
+     * Provides the current size of the list
+     * @return the size of the list
+     */
+    int size();
+
+    /**
+     * Checks if the list is empty
+     * @return true if the list is empty, false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * @return an {@link Iterator} which allows to iterate forward the list in a circular manner.
+     */
+    Iterator<Integer> forwardIterator();
+
+    /**
+     * @return an {@link Iterator} which allows to iterate backward the list in a circular manner.
+     */
+    Iterator<Integer> backwardIterator();
+
+    /**
+     * Reset the position of current element back to the first one of the list.
+     * Note the first one is the first added to the list.
+     */
+    void reset();
+}
